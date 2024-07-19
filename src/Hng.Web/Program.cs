8f103359d98ce3c043916560914ad1faaf71fb93
mroluwasesan <mr.oluwasesan@gmail.com>
2024-07-19 10:48:19 +0100

    commit

---
+++ src/Hng.Web/Program.cs
@@ -0,0 +1,30 @@
+using Hng.Web.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connString = builder.Configuration.GetConnectionString("DefaultConnectionString");
+builder.Services.AddConfiguredServices(connString);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ src/Hng.Web/ServiceExtensions/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Hng.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Hng.Web.Services
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddConfiguredServices(this IServiceCollection services, string connectionString)
+        {
+            services.AddScoped<DbContext, MyDBContext>();
+
+            services.AddDbContext<MyDBContext>(options =>
+                options.UseMySQL(connectionString));
+
+            return services;
+        }
+    }
+}
